cf36581efa81da8e3432f0ddbd7650d7b5fcf34a
Edwin A. Collazos <eacl.831@gmail.com>
2024-03-23 12:17:25 -0400

    A4: adding the two created apps (posts and profiles) and the crispy_forms installed app

--- src/webapp_project/settings.py
+++ src/webapp_project/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # own apps
+    'posts',
+    'profiles',
+    # third party
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
