f5f7e4127eb6360772054feff15c05169aef9aca
Edwin A. Collazos <eacl.831@gmail.com>
2024-03-24 13:35:45 -0400

    A4: add main.html in posts app and extends from base.html

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+def post_list_and_create(request):
+    # we define a query set
+    qs = Post.objects.all()
+    return render(request, 'posts/main.html', {'qs':qs}) 
