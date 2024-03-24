1607713f82b2403ca7629338ee71af9ac992a88a
Edwin A. Collazos <eacl.831@gmail.com>
2024-03-24 13:56:31 -0400

    A4: performing first ajax call

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -1,1 +1,12 @@
 console.log("Hello World");
+const hello_world_box = document.getElementById("hello-word");
+
+$.ajax({
+    type: 'GET',
+    url: '/hello-world/',
+    success: (response) => { 
+        console.log('seucces', response);
+        hello_world_box.textContent = response.text;
+    },
+    error: (err) => { console.log('error', err); },
+})
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import Post
 
 # Create your views here.
@@ -6,4 +7,7 @@ from .models import Post
 def post_list_and_create(request):
     # we define a query set
     qs = Post.objects.all()
-    return render(request, 'posts/main.html', {'qs':qs}) 
+    return render(request, 'posts/main.html', {'qs':qs})
+
+def hello_word_view(request):
+    return JsonResponse({'text': 'Hello World'})
