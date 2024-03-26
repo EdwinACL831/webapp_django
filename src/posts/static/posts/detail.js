bab9b71674cda66fa3679193673a2717809a4372
Edwin A. Collazos <eacl.831@gmail.com>
2024-03-26 00:14:05 -0400

    A4: implements logic to actually update the post from the update form

--- src/posts/static/posts/detail.js
+++ src/posts/static/posts/detail.js
@@ -5,8 +5,15 @@ const spinnerContainer = document.getElementById("spinner-container");
 const postContainer = document.getElementById("post-container");
 const titleInput = document.getElementById("id_title");
 const contentInput = document.getElementById("id_content");
+const updateForm = document.getElementById("update-form");
+const deleteForm = document.getElementById("delete-form");
+const alertContainer = document.getElementById("alert-container");
+
+const csrfToken = document.getElementsByName("csrfmiddlewaretoken");
 
 const url = window.location.href + "data/"
+const updateUrl = window.location.href + "update/"
+const deleteUrl = window.location.href + "delete/"
 
 backBtn.addEventListener("click", () => {
     history.back();
@@ -30,9 +37,11 @@ $.ajax({
 
         const titleEl = document.createElement("h3");
         titleEl.setAttribute("class", "mt-3");
+        titleEl.setAttribute("id", "title");
 
         const contentEl = document.createElement("p");
         contentEl.setAttribute("class", "mt-1");
+        contentEl.setAttribute("id", "content");
 
         titleEl.textContent = data.title;
         contentEl.textContent = data.content;
@@ -46,4 +55,29 @@ $.ajax({
     error: (err) => {
         console.log(err);
     }
+})
+
+updateForm.addEventListener("submit", e =>{
+    e.preventDefault();
+    const title = document.getElementById("title");
+    const content = document.getElementById("content");
+
+    $.ajax({
+        type: "POST",
+        url: updateUrl,
+        data: {
+            "csrfmiddlewaretoken": csrfToken[0].value,
+            "title": titleInput.value,
+            "content": contentInput.value,
+        },
+        success: (response) => {
+            console.log(response);
+            handleAlerts("success", "Post has been updated!");
+            title.textContent = response.title;
+            content.textContent = response.content;
+        },
+        error: err => {
+            console.log(err);
+        }
+    })
 })
