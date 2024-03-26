ab1dba80f9e82674586091aabefdad09d68b186a
Edwin A. Collazos <eacl.831@gmail.com>
2024-03-25 23:19:26 -0400

    A4: adds logic to display delete and update buttons on detail.html when the author is equal to logged_in

--- src/posts/static/posts/detail.js
+++ src/posts/static/posts/detail.js
@@ -1,4 +1,6 @@
 const backBtn = document.getElementById("back-btn");
+const updateBtn = document.getElementById("update-btn");
+const deleteBtn = document.getElementById("delete-btn");
 const spinnerContainer = document.getElementById("spinner-container");
 
 const url = window.location.href + "data/"
@@ -13,6 +15,15 @@ $.ajax({
     success: (response) => {
         console.log(response);
         spinnerContainer.classList.add("not-visible");
+        const data = response.data;
+
+        if(data.logged_in !== data.author) {
+            console.log("different");
+        } else {
+            console.log("the same");
+            updateBtn.classList.remove("not-visible");
+            deleteBtn.classList.remove("not-visible");
+        }
     },
     error: (err) => {
         console.log(err);
