a42da5dff6b254d847feecc90425c17d313600af
Edwin A. Collazos <eacl.831@gmail.com>
2024-03-26 16:09:36 -0400

    A4: create view and form related to profiles app

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from .models import Profile
+from .forms import ProfileForm
+from django.http import JsonResponse
 
 # Create your views here.
+
+def my_profile_view(request):
+    obj = Profile.objects.get(user=request.user)
+    form = ProfileForm(request.POST or None, request.FILES or None, instance=obj)
+    # this is the "new" way to check for the AJAX request method
+    # instead of request.is_ajax() -> https://docs.djangoproject.com/en/4.0/releases/3.1/#id2
+    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+        if form.is_valid():
+            instance = form.save()
+            return JsonResponse({
+                'bio': instance.bio,
+                'avatar': instance.avatar.url,
+                'user': instance.user.username
+            })
+
+    context = {
+        'obj': obj,
+        'form': form,
+    }
+
+    return render(request, 'profiles/main.html', context)
